99b21206ea3d512a8156e016a6877d250aecb7a7
Eryk Latasiewicz <eryk.ne05@gmail.com>
2022-10-14 12:38:35 +0200

    update for stats.js and form.js

--- word-counter/src/components/Form.js
+++ word-counter/src/components/Form.js
@@ -1,33 +1,34 @@
-import React, { useState, useEffect } from 'react'
-import TextField from './TextField'
-import styles from './Form.module.scss'
-import Stats from './Stats'
+import React, { useState, useEffect } from "react";
+import TextField from "./TextField";
+import styles from "./Form.module.scss";
+import Stats from "./Stats";
 
 const Form = () => {
-  const [text, setText] = useState('')
-  const [words, setWords] = useState([])
-  const [wordsNum, setWordsNum] = useState(0)
-  const characters = text.length
-
+  const [text, setText] = useState("");
+  const [words, setWords] = useState([]);
+  const [wordsNum, setWordsNum] = useState(0);
+  const characters = text.length;
 
   useEffect(() => {
-    if(text.length > 0) {
-      setWords(text.split(' '))
-      setWordsNum(words.length)
-      setWords(prevState => prevState.filter(word => word != ''))
-      console.log(words)
-      console.log(wordsNum)
+    if (text.length > 0) {
+      setWords(text.split(" "));
+      setWordsNum(words.length);
+      setWords((prevState) => prevState.filter((word) => word !== ""));
+      console.log(words);
+      console.log(wordsNum);
     }
-  }, [text])
-  
-  return (
-    <div>
-      <Stats characters={characters} words={wordsNum} />
-      <form action="#" className={styles.form}>
-        <TextField setText={setText} text={text} />
-      </form>
-    </div>
-  )
-}
+  }, [text]);
 
-export default Form
+  return (
+    <form
+      action="#"
+      className={styles.form}
+      onSubmit={(e) => e.preventDefault()}
+    >
+      <TextField setText={setText} text={text} />
+      <Stats characters={characters} words={wordsNum} setText={setText} />
+    </form>
+  );
+};
+
+export default Form;
--- word-counter/src/components/Stats.js
+++ word-counter/src/components/Stats.js
@@ -1,13 +1,19 @@
-import React from 'react'
-import styles from './Stats.module.scss';
+import React from "react";
+import styles from "./Stats.module.scss";
 
-const Stats = ({characters, words}) => {
+const Stats = ({ characters, words, setText }) => {
   return (
     <div className={styles.wrapper}>
+      <div>
         <span className={styles.stat}>{words} words</span>
         <span className={styles.stat}>{characters} characters</span>
+      </div>
+      <div>
+        <button>Select All</button>
+        <button>Reset</button>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Stats
+export default Stats;
